470e1b59aabc7ff21882fdab6c600728a6fe051b
Vinayak <vinayalaw@gmail.com>
2024-10-19 09:58:05 -0400

    update status and timing logic

--- DAILY_NBA_Import.py
+++ DAILY_NBA_Import.py
@@ -49,66 +49,69 @@ def saveGameData(game, team):
     gameScores = None
     gameDate = utilFunctions.getDateFromString(game['date']['start'])
 
-    if(curTeamID == homeTeamID):
-        home = 1
-        gameScores = game['scores']['home']
-    else:
-        gameScores = game['scores']['visitors']
-    insertSQL = '''INSERT INTO tbl_games (game_id,league,season,
-    date,teamName,teamCode,home,points,q1score,q2score,q3score,
-    q4score,plusminus,fastBreakPoints,pointsInPaint,biggestLead,
-    secondChancePoints,pointsOffTurnOvers,LongestRun,fgm,fga,fgp,
-    ftm,fta,ftp,tpm,tpa,tpp,offReb,defReb,totReb,assists,pFouls,
-    steals,turnovers,blocks) VALUES ('''
-    insertSQL += str(game['id']) + ","
-    insertSQL += "'" + game['league'] + "',"
-    insertSQL += str(game['season']) + ","
-    insertSQL += "'" + str(gameDate) + "',"
-    insertSQL += "'" + team['team']['name'] + "',"
-    insertSQL += "'" + team['team']['code'] + "',"
-    insertSQL += convertStr(home) + ","
-    insertSQL += convertStr(gameScores['points']) + ","
-    try:
-        q1Score = convertStr(gameScores['linescore'][0])
-        q2Score = convertStr(gameScores['linescore'][1])
-        q3Score = convertStr(gameScores['linescore'][2])
-        q4Score = convertStr(gameScores['linescore'][3])
-        insertSQL += q1Score + ","
-        insertSQL += q2Score + ","
-        insertSQL += q3Score + ","
-        insertSQL += q4Score + ","
-    except:
-        with open(errorLogPath, "a") as myfile:
-            myfile.write("\nFaulty LineScore for GameID:\n" + str(game['id']))
-    insertSQL += convertStr(teamStats['plusMinus']) + ","
-    insertSQL += convertStr(teamStats['fastBreakPoints']) + ","
-    insertSQL += convertStr(teamStats['pointsInPaint']) + ","
-    insertSQL += convertStr(teamStats['biggestLead']) + ","
-    insertSQL += convertStr(teamStats['secondChancePoints']) + ","
-    insertSQL += convertStr(teamStats['pointsOffTurnovers']) + ","
-    insertSQL += convertStr(teamStats['longestRun']) + ","
-    insertSQL += convertStr(teamStats['fgm']) + ","
-    insertSQL += convertStr(teamStats['fga']) + ","
-    insertSQL += convertStr(teamStats['fgp']) + ","
-    insertSQL += convertStr(teamStats['ftm']) + ","
-    insertSQL += convertStr(teamStats['fta']) + ","
-    insertSQL += convertStr(teamStats['ftp']) + ","
-    insertSQL += convertStr(teamStats['tpm']) + ","
-    insertSQL += convertStr(teamStats['tpa']) + ","
-    insertSQL += convertStr(teamStats['tpa']) + ","
-    insertSQL += convertStr(teamStats['offReb']) + ","
-    insertSQL += convertStr(teamStats['defReb']) + ","
-    insertSQL += convertStr(teamStats['totReb']) + ","
-    insertSQL += convertStr(teamStats['assists']) + ","
-    insertSQL += convertStr(teamStats['pFouls']) + ","
-    insertSQL += convertStr(teamStats['steals']) + ","
-    insertSQL += convertStr(teamStats['turnovers']) + ","
-    insertSQL += convertStr(teamStats['blocks']) + ");"
-    try:
-        utilFunctions.executeNonQuery(insertSQL)
-    except:
-        with open(errorLogPath, "a") as myfile:
-            myfile.write("\nFaulty Insert:\n"+insertSQL)
+    selectSQL = "SELECT * FROM tbl_games WHERE game_id = " + str(game['id']) + " AND teamCode = '" + team['team']['code'] + "'"
+    result = utilFunctions.selectQuery(selectSQL)
+    if(len(result)==0):
+        if(curTeamID == homeTeamID):
+            home = 1
+            gameScores = game['scores']['home']
+        else:
+            gameScores = game['scores']['visitors']
+        insertSQL = '''INSERT INTO tbl_games (game_id,league,season,
+        date,teamName,teamCode,home,points,q1score,q2score,q3score,
+        q4score,plusminus,fastBreakPoints,pointsInPaint,biggestLead,
+        secondChancePoints,pointsOffTurnOvers,LongestRun,fgm,fga,fgp,
+        ftm,fta,ftp,tpm,tpa,tpp,offReb,defReb,totReb,assists,pFouls,
+        steals,turnovers,blocks) VALUES ('''
+        insertSQL += str(game['id']) + ","
+        insertSQL += "'" + game['league'] + "',"
+        insertSQL += str(game['season']) + ","
+        insertSQL += "'" + str(gameDate) + "',"
+        insertSQL += "'" + team['team']['name'] + "',"
+        insertSQL += "'" + team['team']['code'] + "',"
+        insertSQL += convertStr(home) + ","
+        insertSQL += convertStr(gameScores['points']) + ","
+        try:
+            q1Score = convertStr(gameScores['linescore'][0])
+            q2Score = convertStr(gameScores['linescore'][1])
+            q3Score = convertStr(gameScores['linescore'][2])
+            q4Score = convertStr(gameScores['linescore'][3])
+            insertSQL += q1Score + ","
+            insertSQL += q2Score + ","
+            insertSQL += q3Score + ","
+            insertSQL += q4Score + ","
+        except:
+            with open(errorLogPath, "a") as myfile:
+                myfile.write("\nFaulty LineScore for GameID:\n" + str(game['id']))
+        insertSQL += convertStr(teamStats['plusMinus']) + ","
+        insertSQL += convertStr(teamStats['fastBreakPoints']) + ","
+        insertSQL += convertStr(teamStats['pointsInPaint']) + ","
+        insertSQL += convertStr(teamStats['biggestLead']) + ","
+        insertSQL += convertStr(teamStats['secondChancePoints']) + ","
+        insertSQL += convertStr(teamStats['pointsOffTurnovers']) + ","
+        insertSQL += convertStr(teamStats['longestRun']) + ","
+        insertSQL += convertStr(teamStats['fgm']) + ","
+        insertSQL += convertStr(teamStats['fga']) + ","
+        insertSQL += convertStr(teamStats['fgp']) + ","
+        insertSQL += convertStr(teamStats['ftm']) + ","
+        insertSQL += convertStr(teamStats['fta']) + ","
+        insertSQL += convertStr(teamStats['ftp']) + ","
+        insertSQL += convertStr(teamStats['tpm']) + ","
+        insertSQL += convertStr(teamStats['tpa']) + ","
+        insertSQL += convertStr(teamStats['tpa']) + ","
+        insertSQL += convertStr(teamStats['offReb']) + ","
+        insertSQL += convertStr(teamStats['defReb']) + ","
+        insertSQL += convertStr(teamStats['totReb']) + ","
+        insertSQL += convertStr(teamStats['assists']) + ","
+        insertSQL += convertStr(teamStats['pFouls']) + ","
+        insertSQL += convertStr(teamStats['steals']) + ","
+        insertSQL += convertStr(teamStats['turnovers']) + ","
+        insertSQL += convertStr(teamStats['blocks']) + ");"
+        try:
+            utilFunctions.executeNonQuery(insertSQL)
+        except:
+            with open(errorLogPath, "a") as myfile:
+                myfile.write("\nFaulty Insert:\n"+insertSQL)
     return None
 
 #FUNCTION getLastDateCheck - checks sql table for last date api was checked for game data
@@ -151,51 +154,58 @@ def getNbaGameDay(headers, baseurl, datestring):
 def importNBAGameData():
     lastDateChecked = getLastDateCheck()
     todaysDate = datetime.date.today()
-    while lastDateChecked < todaysDate:
-        lastDateChecked += datetime.timedelta(days=1)
+    while lastDateChecked < (todaysDate):
         dailyGames = getNbaGameDay(headers, config_data['api_host'], utilFunctions.ConvertDateToString(lastDateChecked))
+        i = 0
         for game in dailyGames:
-            gameStats = getGameStats(headers, config_data['api_host'], game)
-            for team in gameStats:
-                saveGameData(game, team)
+            status = game.get("status").get("long")
+            #print(game.get("date").get("start"))
+            #print(status)
+            #print(game.get("teams").get("visitors").get("code"))
+            #print(game.get("teams").get("home").get("code"))
+            if(status == "Finished"):
+                gameStats = getGameStats(headers, config_data['api_host'], game)
+                for team in gameStats:
+                    saveGameData(game, team)
+        lastDateChecked += datetime.timedelta(days=1)
     if(getLastDateCheck() < todaysDate): 
-        setLastDateCheck(utilFunctions.ConvertDateToString(todaysDate))
+        setLastDateCheck(utilFunctions.ConvertDateToString(todaysDate-datetime.timedelta(days=1)))
 
 def predictTodaysGames():
     model, featureEncoder, featureData, predictColumns = utilNBA_ML_RFR.getModelAndColumns()
     today = datetime.date.today()
     todaysGames = getNbaGameDay(headers, config_data['api_host'], utilFunctions.ConvertDateToString(today))
     for game in todaysGames:
-        game_id = game.get("id")
-        home_team = game.get("teams").get("home").get("code")
-        visitor_team = game.get("teams").get("visitors").get("code")
-        input_data = pd.DataFrame({
-            'homeCode': [home_team],
-            'visitorCode': [visitor_team],
-            'Year': [today.year],
-            'Month': [today.month],
-            'Day': [today.day]
-        })
-        input_encoded = featureEncoder.transform(input_data[['homeCode', 'visitorCode']])
-        input_encoded_df = pd.DataFrame(input_encoded, columns=featureEncoder.get_feature_names_out(['homeCode', 'visitorCode']))
-        input_full = pd.concat([input_data[['Year', 'Month', 'Day']], input_encoded_df], axis=1)
-        input_full = input_full[featureData.columns]
-        prediction = model.predict(input_full)
-        predictTable = [predictColumns,prediction]
-        wantedColumns = ['homePoints','homeplusminus','visitorPoints','visitorplusminus','totalPoints']
-        predictionDict = {
-            'game_id' : game_id,
-            'date' : utilFunctions.ConvertDateToString(today),
-            'homeCode': home_team,
-            'visitorCode': visitor_team
-        }
-        i = 0
-        while i < len(wantedColumns):
-            predictionDict.update({wantedColumns[i]:prediction[0,predictColumns.index(wantedColumns[i])]})
-            i += 1
-        print("\n")
-        printDict(predictionDict)
-        savePrediction(predictionDict)
+        if(game.get("status").get("long") == "Finished"):
+            game_id = game.get("id")
+            home_team = game.get("teams").get("home").get("code")
+            visitor_team = game.get("teams").get("visitors").get("code")
+            input_data = pd.DataFrame({
+                'homeCode': [home_team],
+                'visitorCode': [visitor_team],
+                'Year': [today.year],
+                'Month': [today.month],
+                'Day': [today.day]
+            })
+            input_encoded = featureEncoder.transform(input_data[['homeCode', 'visitorCode']])
+            input_encoded_df = pd.DataFrame(input_encoded, columns=featureEncoder.get_feature_names_out(['homeCode', 'visitorCode']))
+            input_full = pd.concat([input_data[['Year', 'Month', 'Day']], input_encoded_df], axis=1)
+            input_full = input_full[featureData.columns]
+            prediction = model.predict(input_full)
+            predictTable = [predictColumns,prediction]
+            wantedColumns = ['homePoints','homeplusminus','visitorPoints','visitorplusminus','totalPoints']
+            predictionDict = {
+                'game_id' : game_id,
+                'date' : utilFunctions.ConvertDateToString(today),
+                'homeCode': home_team,
+                'visitorCode': visitor_team
+            }
+            i = 0
+            while i < len(wantedColumns):
+                predictionDict.update({wantedColumns[i]:prediction[0,predictColumns.index(wantedColumns[i])]})
+                i += 1
+            #printDict(predictionDict)
+            savePrediction(predictionDict)
             
 def savePrediction(predictionDict):
     selectSQL = "SELECT * FROM tbl_predictions WHERE game_id = " + convertStr(predictionDict.get("game_id"))
@@ -219,6 +229,7 @@ def savePrediction(predictionDict):
         return None
 
 def printDict(dict):
+    print("\n")
     for key, value in dict.items():
         print(f"{key}: {value}")
         
